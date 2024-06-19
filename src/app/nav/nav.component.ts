952f516204f28363e71dcc33b2b2ca70e205ac45
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-06-19 23:53:16 +0530

    nav bar created

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  navLinks: string[] = ['Home', 'About', 'Gallery',
+                       'Contact', 'Pricing', 'Classes',];
+}
