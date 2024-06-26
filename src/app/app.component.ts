4fe9a37e51c2ff52b39094e33bbd51a46565b78a
Harshitha-0306 <harshitha.y2022@gmail.com>
2024-06-26 11:16:27 +0530

    component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
-import { HeroComponent } from './hero/hero.component';
-
+import { ButtonComponent } from './button/button.component';
+;
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent, HeroComponent],
+  imports: [RouterOutlet, NavComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
