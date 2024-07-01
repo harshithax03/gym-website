7a58f0cfe47c4d29f7863fe1444710146da3c8ec
harshitha <harshitha.y2022@gmail.com>
2024-07-01 06:14:56 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
+  numbers(n:number): number[]{
+    return Array(n);
+  }
   
 }
